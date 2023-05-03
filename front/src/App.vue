a3e47501b7c438276f000b0ffbe65e68bad6aa25
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-03 14:46:23 +0200

    dev: added layout

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import BodyLayout from './components/layout/BodyLayout.vue';
+import FooterLayout from './components/layout/FooterLayout.vue';
+import HeaderLayout from './components/layout/HeaderLayout.vue';
+
 
 </script>
 
 <template>
-  <p>Gestion Stock</p>
+<HeaderLayout/>
+<BodyLayout/>
+<FooterLayout/>
 </template>
 
 <style scoped>
---
+++ front/src/components/layout/HeaderLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>Header works!</p>
+</template>
+
+<style scoped>
+
+</style>
