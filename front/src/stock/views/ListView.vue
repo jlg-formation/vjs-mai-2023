77cdfdc24bb40e2476e70b9ed80a187e01f747fa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-04 11:45:14 +0200

    dev: relative path

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -6,7 +6,7 @@
     <div class="content">
       <nav>
         <button>Rafraîchir</button>
-        <RouterLink to="./add" class="button">Ajouter</RouterLink>
+        <RouterLink :to="$route.path + '/add'" class="button">Ajouter</RouterLink>
         <button>Supprimer</button>
       </nav>
       <table>
