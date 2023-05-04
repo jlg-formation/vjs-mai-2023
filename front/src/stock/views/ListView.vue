20952771bd4afc6a30547fc00b819446ba7f6573
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-04 15:37:25 +0200

    dev: added interface Article

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-const articles = [
+import type { Article } from '@/interfaces/Article'
+
+const articles: Article[] = [
   { id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 },
   { id: 'a2', name: 'Pelle', price: 3, qty: 4 },
   { id: 'a3', name: 'Marteau', price: 1, qty: 62 }
