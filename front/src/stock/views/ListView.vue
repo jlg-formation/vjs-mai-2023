8e62e290e2874d06c3378436efd3a09f9df11931
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-04 10:49:43 +0200

    dev: added list view dom

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -1,7 +1,47 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>List View</p>
+  <main>
+    <h1>Liste des articles</h1>
+    <div class="content">
+      <nav>
+        <button>Rafraîchir</button>
+        <RouterLink to="./add" class="button">Ajouter</RouterLink>
+        <button>Supprimer</button>
+      </nav>
+      <table>
+        <thead>
+          <tr>
+            <th class="name">Nom</th>
+            <th class="price">Prix</th>
+            <th class="qty">Quantité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="name">Tournevis</td>
+            <td class="price">3.50 €</td>
+            <td class="qty">450</td>
+          </tr>
+          <tr>
+            <td class="name">Pelle</td>
+            <td class="price">2.50 €</td>
+            <td class="qty">23</td>
+          </tr>
+          <tr>
+            <td class="name">Rateau</td>
+            <td class="price">10.00 €</td>
+            <td class="qty">100</td>
+          </tr>
+          <tr>
+            <td class="name">Marteau</td>
+            <td class="price">4.60 €</td>
+            <td class="qty">8</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss"></style>
