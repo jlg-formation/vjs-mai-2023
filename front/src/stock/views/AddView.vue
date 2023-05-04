231c373fb90bfbe7dee912f687a32af6cf7b362a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-04 11:56:46 +0200

    dev: added dom and css for addview

--- front/src/stock/views/AddView.vue
+++ front/src/stock/views/AddView.vue
@@ -1,7 +1,48 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>Add View</p>
+  <main>
+    <h1>Ajouter un article</h1>
+    <form>
+      <label>
+        <span>Nom</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Prix</span>
+        <input type="number" />
+      </label>
+      <label>
+        <span>Quantité</span>
+        <input type="number" />
+      </label>
+      <button class="primary">Ajouter</button>
+    </form>
+  </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+form {
+  display: flex;
+  flex-flow: column;
+  gap: 0.5em;
+
+  width: 100%;
+  max-width: 25em;
+
+  label {
+    display: flex;
+    flex-flow: column;
+
+    input {
+      border: 0.1em solid var(--aaa);
+      border-radius: 0.3em;
+      padding: 0.5em 1em;
+    }
+  }
+
+  button {
+    margin-top: 3em;
+  }
+}
+</style>
