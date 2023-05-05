75656f6e5606dae84e235825fae71f73e9bae1e7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-05 17:08:59 +0200

    dev: url rewriting

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,7 +5,7 @@ import { api } from "./api";
 console.log("Starting web server...");
 
 const port = 3000;
-const publicDir = ".";
+const publicDir = "../front/dist";
 
 const app = express();
 
@@ -13,6 +13,11 @@ app.use("/api", api);
 
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
+
+// URL Rewriting
+app.get("**", (req, res) => {
+  res.sendFile("index.html", { root: publicDir });
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
